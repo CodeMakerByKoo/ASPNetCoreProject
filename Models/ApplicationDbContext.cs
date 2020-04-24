29fe3d9a4a5c4d6b9fba3d8d7684d4da50da128b
CodeMakerByKoo <koo_by@hotmail.com>
2020-04-24 15:38:43 +0800

    ASP.NET Core CRUD

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core_Crud_MVC.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<NewEmpClass> EmployeeTable { get; set; }
+    }
+}
